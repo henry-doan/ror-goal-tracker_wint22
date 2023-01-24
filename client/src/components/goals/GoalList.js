90b88f57ea1637e29bd7f510e65f01bc5998149b
Henry Doan <henrydoan96@gmail.com>
2023-01-23 18:53:30 -0700

    add animations

--- client/src/components/goals/GoalList.js
+++ client/src/components/goals/GoalList.js
@@ -1,21 +1,24 @@
 import { CardCol, CardImg, CardRow, MainCard, MainCardContainer } from "../styles/shared";
 import { Link } from 'react-router-dom';
+import { Fade } from 'react-awesome-reveal';
 
 const GoalConsumer = ({ goals }) => (
   <CardRow>
     { goals.map( g =>
-        <CardCol key={g.id}>
-          <Link 
-            to={`/goals/${g.id}`}
-            state={{ ...g }}
-          >
-          <MainCard>
-            <CardImg src={g.img} width={'100%'} alt={g.target} />
-            <MainCardContainer>
-              <h1>{g.target}</h1>
-              <p>Created By: {g.author}</p>
-            </MainCardContainer>
-          </MainCard>
+      <CardCol key={g.id}>
+        <Link 
+          to={`/goals/${g.id}`}
+          state={{ ...g }}
+        >
+          <Fade>
+            <MainCard>
+              <CardImg src={g.img} width={'100%'} alt={g.target} />
+              <MainCardContainer>
+                <h1>{g.target}</h1>
+                <p>Created By: {g.author}</p>
+              </MainCardContainer>
+            </MainCard>
+          </Fade>
         </Link>
       </CardCol>
     )}
--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,14 +1,23 @@
 import { Link } from 'react-router-dom';
 import { MainButton, HomeHeader, HomeHeaderTxt } from '../styles/shared';
+import { Slide } from 'react-awesome-reveal';
 
 const Home = () => (
   <HomeHeader>
     <HomeHeaderTxt>
       <Link to='/login'>
-        <MainButton className='main-font'>Login</MainButton>
+        <MainButton className='main-font'>
+          <Slide direction='up'>
+            Login
+          </Slide>
+        </MainButton>
       </Link>
       <Link to='/register'>
-        <MainButton className='main-font'>SignUp</MainButton>
+        <MainButton className='main-font'>
+          <Slide direction='up'>
+            SignUp
+          </Slide>
+        </MainButton>
       </Link>
     </HomeHeaderTxt>
   </HomeHeader>
